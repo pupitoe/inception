ec69d8bd966e78fc0154ea73671d0caeefab7136
tlassere <tlassere@student.42mulhouse.fr>
2024-07-08 14:33:37 -0600

    Feat: SSL

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,6 +1,17 @@
 #! /bin/sh
 
-$c_nginx=/etc/nginx/certif
+c_nginx=/etc/nginx/certif
 
-mkdir 
-openssl genrsa -out fuck.key 2048
+namefile=fuck
+
+c_path="$c_nginx/$namefile"
+
+mkdir "$c_nginx"
+#if [ $(ls $c_path.key) -eq 1 ]; then
+	openssl genrsa -out $c_path.key 2048
+	openssl req -new -key $c_path.key -out $c_path.csr -nodes -subj "/C=42/ST=France/L=Mulhouse/O=tomcompany/OU=tomCompanySection/CN=Inception"
+	openssl x509 -req -days 3650 -in $c_path.csr -signkey $c_path.key -out $c_path.crt
+#fi
+
+#execut arguments
+exec "$@"
