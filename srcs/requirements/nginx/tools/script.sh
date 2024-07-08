ee7d36f9023205bbbce56d7727b51ec26c7181d0
tlassere <tlassere@student.42mulhouse.fr>
2024-07-08 15:28:52 -0600

    Feat: j'ai essayer mais j'ai pas reussi

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -7,11 +7,9 @@ namefile=fuck
 c_path="$c_nginx/$namefile"
 
 mkdir "$c_nginx"
-#if [ $(ls $c_path.key) -eq 1 ]; then
-	openssl genrsa -out $c_path.key 2048
-	openssl req -new -key $c_path.key -out $c_path.csr -nodes -subj "/C=42/ST=France/L=Mulhouse/O=tomcompany/OU=tomCompanySection/CN=Inception"
-	openssl x509 -req -days 3650 -in $c_path.csr -signkey $c_path.key -out $c_path.crt
-#fi
+openssl genrsa -out $c_path.key 2048
+openssl req -new -key $c_path.key -out $c_path.csr -nodes -subj "/C=42/ST=France/L=Mulhouse/O=tomcompany/OU=tomCompanySection/CN=Inception"
+openssl x509 -req -days 3650 -in $c_path.csr -signkey $c_path.key -out $c_path.crt
 
 #execut arguments
 exec "$@"
