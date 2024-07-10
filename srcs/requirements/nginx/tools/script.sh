f4087454a8d00db947626a6b22e2d66fd2e4b5b0
tlassere <tlassere@student.42mulhouse.fr>
2024-07-10 07:43:49 -0600

    Fix: script for nginx and begin maria db

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -6,13 +6,28 @@ namefile=fuck
 
 c_path="$c_nginx/$namefile"
 
-mkdir "$c_nginx"
+mkdir -p "$c_nginx"
+
+# the conditions are used to check if the files are not created and to make them as needed
+# -e return true if file exist
+
 # make a key rsa
-openssl genrsa -out $c_path.key 2048
-# generate a certificate request
-openssl req -new -key $c_path.key -out $c_path.csr -nodes -subj "/C=42/ST=France/L=Mulhouse/O=tomcompany/OU=tomCompanySection/CN=Inception"
+if [ ! -e $c_path.key ];
+then
+	openssl genrsa -out $c_path.key 2048
+fi
+
+# generate a certificate request (-nodes -subj is used to not use interactive mode and make all inline)
+if [ ! -e $c_path.csr ];
+then
+	openssl req -new -key $c_path.key -out $c_path.csr -nodes -subj "/C=42/ST=France/L=Mulhouse/O=tomcompany/OU=tomCompanySection/CN=Inception"
+fi
+
 # creating the certificate from the key and the request (-req specifies that it is a sertificate request)
-openssl x509 -req -days 3650 -in $c_path.csr -signkey $c_path.key -out $c_path.crt
+if [ ! -e $c_path.crt ];
+then
+	openssl x509 -req -days 3650 -in $c_path.csr -signkey $c_path.key -out $c_path.crt
+fi
 
 #execut arguments
 exec "$@"
