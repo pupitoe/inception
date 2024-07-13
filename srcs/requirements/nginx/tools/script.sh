5c3ec6b45d21f63d17748839dd704911ecd96003
tlassere <tlassere@student.42mulhouse.fr>
2024-07-13 06:42:06 -0600

    Feat: nginx with phpfpm

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -27,9 +27,9 @@ then
 	openssl x509 -req -days 3650 -in $c_path.csr -signkey $c_path.key -out $c_path.crt
 fi
 
-#while ! nc -z localhost 6969; do
-#  sleep 0.1
-#done
+while ! nc -z wordpress 9000; do
+  sleep 0.1
+done
 
 #execut arguments
 exec "$@"
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+while ! nc -z mariadb 3306; do
+  sleep 0.1
+done
+
+exec "$@"
